267739dd4ecdb7f886f0bdfd6a938a57dc7264e3
Yosri Hammadi <yosri.hammadi@gmail.com>
2021-11-10 17:02:46 +0100

    add stripe to api

---
+++ api/routes/stripe.js
@@ -0,0 +1,20 @@
+module.exports = (app) => {
+	const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+	app.post("/payment", (req, res) => {
+		stripe.charges.create(
+			{
+				source: req.body.tokenId,
+				amount: req.body.amount,
+				currency: "usd",
+			},
+			(stripeErr, stripeRes) => {
+				if (stripeErr) {
+					res.status(500).json(stripeErr);
+				} else {
+					res.status(200).json(stripeRes);
+				}
+			}
+		);
+	});
+};
