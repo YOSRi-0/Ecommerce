183c6695a0d22b112b5c0b0b64c620b322b3776b
Yosri Hammadi <yosri.hammadi@gmail.com>
2021-11-10 10:30:28 +0100

    add verifyToken to cart and order routes

--- api/routes/cart.routes.js
+++ api/routes/cart.routes.js
@@ -1,18 +1,27 @@
 module.exports = (app) => {
-    const cart = require('../controllers/cart.controller.js');
+	const cart = require("../controllers/cart.controller.js");
+	const { verifyTokenAndAuthorization } = require("./verifyToken");
 
-    // Create a new Cart
-    app.post('/cart', cart.create);
+	// Create a new Cart
+	app.post("/cart", cart.create);
 
-    // Get cart
-    app.get('/cart/:id', cart.findByUserId);
+	// Get cart
+	app.get("/cart/:id", verifyTokenAndAuthorization, cart.findByUserId);
 
-    // Add To cart
-    app.post('/cart/add', cart.addToCart);
+	// Add To cart
+	app.post("/cart/add", verifyTokenAndAuthorization, cart.addToCart);
 
-    // Delete from cart
-    app.delete('/cart/delete', cart.deleteProductFromCart);
+	// Delete from cart
+	app.delete(
+		"/cart/delete",
+		verifyTokenAndAuthorization,
+		cart.deleteProductFromCart
+	);
 
-    // Delete all products from cart
-    app.delete('/cart/delete/all/:id', cart.deleteAll);
+	// Delete all products from cart
+	app.delete(
+		"/cart/delete/all/:id",
+		verifyTokenAndAuthorization,
+		cart.deleteAll
+	);
 };
--- api/routes/order.routes.js
+++ api/routes/order.routes.js
@@ -1,7 +1,11 @@
 module.exports = (app) => {
-    const order = require('../controllers/order.controller');
+	const order = require("../controllers/order.controller");
+	const {
+		verifyTokenAndAuthorization,
+		verifyTokenAndAdmin,
+	} = require("./verifyToken");
 
-    app.post('/orders', order.create);
+	app.post("/orders", verifyTokenAndAuthorization, order.create);
 
-    app.get('/orders', order.getOrders);
+	app.get("/orders", verifyTokenAndAdmin, order.getOrders);
 };
