4a61553958f4e82ae7e72f27220e8f0f13177f1c
Yosri Hammadi <yosri.hammadi@gmail.com>
2021-11-13 14:06:46 +0100

    add methods to get sizes and colors from api

--- src/components/filters/Filters.js
+++ src/components/filters/Filters.js
@@ -1,52 +1,72 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import {
-    Container,
-    Wrapper,
-    FilterContainer,
-    Filter,
-    FilterText,
-    Select,
-    Option,
-} from './FiltersElements';
+  Container,
+  Wrapper,
+  FilterContainer,
+  Filter,
+  FilterText,
+  Select,
+  Option,
+} from "./FiltersElements";
+import { publicRequest } from "../../requestMethod";
 
 export const Filters = () => {
-    return (
-        <Container>
-            <Wrapper>
-                <FilterContainer>
-                    <Filter>
-                        <FilterText>Filter Products</FilterText>
-                        <Select>
-                            <Option disabled selected>
-                                Color
-                            </Option>
-                            <Option>Black</Option>
-                            <Option>White</Option>
-                            <Option>Yellow</Option>
-                            <Option>Red</Option>
-                            <Option>Green</Option>
-                        </Select>
-                        <Select>
-                            <Option disabled selected>
-                                Size
-                            </Option>
-                            <Option>xs</Option>
-                            <Option>s</Option>
-                            <Option>m</Option>
-                            <Option>l</Option>
-                            <Option>xl</Option>
-                        </Select>
-                    </Filter>
-                    <Filter>
-                        <FilterText>Sort Products</FilterText>
-                        <Select>
-                            <Option>newest</Option>
-                            <Option>price (asc)</Option>
-                            <Option>price (desc)</Option>
-                        </Select>
-                    </Filter>
-                </FilterContainer>
-            </Wrapper>
-        </Container>
-    );
+  const [colors, setColors] = useState([]);
+  const [sizes, setSizes] = useState([]);
+
+  useEffect(() => {
+    const getColors = async () => {
+      try {
+        const res = await publicRequest.get("/colors");
+        setColors(res.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getColors();
+    const getSizes = async () => {
+      try {
+        const res = await publicRequest.get("/sizes");
+        setSizes(res.data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getSizes();
+  }, []);
+  return (
+    <Container>
+      <Wrapper>
+        <FilterContainer>
+          <Filter>
+            <FilterText>Filter Products</FilterText>
+            <Select>
+              <Option disabled selected>
+                Color
+              </Option>
+              {colors.map((color) => (
+                <Option>{color}</Option>
+              ))}
+            </Select>
+            <Select>
+              <Option disabled selected>
+                Size
+              </Option>
+              {sizes.map((size) => (
+                <Option>{size}</Option>
+              ))}
+            </Select>
+          </Filter>
+          <Filter>
+            <FilterText>Sort Products</FilterText>
+            <Select>
+              <Option>newest</Option>
+              <Option>price (asc)</Option>
+              <Option>price (desc)</Option>
+            </Select>
+          </Filter>
+        </FilterContainer>
+      </Wrapper>
+    </Container>
+  );
 };
