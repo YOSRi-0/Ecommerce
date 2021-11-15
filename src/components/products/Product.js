adb11c00c667f968a0ff2c6f57e09e2f8ea45fd9
Yosri Hammadi <yosri.hammadi@gmail.com>
2021-11-15 12:08:20 +0100

    Link product cart icon in home to cart store

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -3,14 +3,24 @@ import { AiOutlineHeart } from "react-icons/ai";
 import { RiShoppingCart2Line } from "react-icons/ri";
 import { BiSearch } from "react-icons/bi";
 import { Container, Image, Informations, Icon } from "./ProductElements";
+import { useDispatch } from "react-redux";
+import { addProduct } from "../../redux/cartRedux";
 
 export const Product = ({ product }) => {
   const path = `/products/${product.id_product}`;
+  const dispatch = useDispatch();
+  const quantity = 1;
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(product);
+    dispatch(addProduct({ ...product, quantity }));
+  };
   return (
     <Container>
       <Image src={product.img} />
       <Informations>
-        <Icon>
+        <Icon onClick={handleClick}>
           <RiShoppingCart2Line />
         </Icon>
         <Icon to={path}>
--- src/pages/products/Product.js
+++ src/pages/products/Product.js
@@ -1,99 +1,96 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
-    Container,
-    Wrapper,
-    ImgContainer,
-    Img,
-    InfoContainer,
-    Title,
-    Description,
-    Price,
-    FilterContainer,
-    Filter,
-    FilterTitle,
-    FilterColor,
-    FilterSize,
-    FilterSizeOption,
-    AddContainer,
-    AmountContainer,
-    Amount,
-} from './ProductElements';
-import { Button } from '../../components/buttons/Button';
-import { IoIosAdd, IoIosRemove } from 'react-icons/io';
-import { useLocation } from 'react-router-dom';
-import { publicRequest } from '../../requestMethod';
-import { useDispatch } from 'react-redux';
-import { addProduct } from '../../redux/cartRedux';
+  Container,
+  Wrapper,
+  ImgContainer,
+  Img,
+  InfoContainer,
+  Title,
+  Description,
+  Price,
+  FilterContainer,
+  Filter,
+  FilterTitle,
+  FilterColor,
+  FilterSize,
+  FilterSizeOption,
+  AddContainer,
+  AmountContainer,
+  Amount,
+} from "./ProductElements";
+import { Button } from "../../components/buttons/Button";
+import { IoIosAdd, IoIosRemove } from "react-icons/io";
+import { useLocation } from "react-router-dom";
+import { publicRequest } from "../../requestMethod";
+import { useDispatch } from "react-redux";
+import { addProduct } from "../../redux/cartRedux";
 
 export const Product = () => {
-    const location = useLocation();
-    const id = location.pathname.split('/')[2];
-    const [product, setProduct] = useState(null);
-    const [quantity, setQuantity] = useState(1);
-    const dispatch = useDispatch();
+  const location = useLocation();
+  const id = location.pathname.split("/")[2];
+  const [product, setProduct] = useState(null);
+  const [quantity, setQuantity] = useState(1);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const getProduct = async () => {
-            try {
-                const res = await publicRequest.get('/products/' + id);
-                setProduct(res.data);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        getProduct();
-    }, [id]);
-
-    const handleClick = () => {
-        dispatch(addProduct({ ...product, quantity }));
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const res = await publicRequest.get("/products/" + id);
+        setProduct(res.data);
+      } catch (error) {
+        console.log(error);
+      }
     };
+    getProduct();
+  }, [id]);
 
-    return (
-        <Container>
-            <Wrapper>
-                <ImgContainer>
-                    <Img src="https://i.ibb.co/Y0djRHf/product-1.jpg" />
-                </ImgContainer>
-                <InfoContainer>
-                    <Title>{product?.title}</Title>
-                    <Description>{product?.description}</Description>
-                    <Price>$ {product?.price}</Price>
-                    <FilterContainer>
-                        <Filter>
-                            <FilterTitle>Color</FilterTitle>
-                            <FilterColor color="black"></FilterColor>
-                            <FilterColor color="tomato"></FilterColor>
-                            <FilterColor color="cornflowerblue"></FilterColor>
-                        </Filter>
-                        <Filter>
-                            <FilterTitle>Size</FilterTitle>
-                            <FilterSize>
-                                <FilterSizeOption>S</FilterSizeOption>
-                                <FilterSizeOption>XS</FilterSizeOption>
-                                <FilterSizeOption>M</FilterSizeOption>
-                                <FilterSizeOption>L</FilterSizeOption>
-                                <FilterSizeOption>XL</FilterSizeOption>
-                            </FilterSize>
-                        </Filter>
-                    </FilterContainer>
-                    <AddContainer>
-                        <AmountContainer>
-                            <IoIosRemove
-                                onClick={() =>
-                                    quantity > 1 && setQuantity(quantity - 1)
-                                }
-                            />
-                            <Amount>{quantity}</Amount>
-                            <IoIosAdd
-                                onClick={() => setQuantity(quantity + 1)}
-                            />
-                        </AmountContainer>
-                        <Button theme="dark" onClick={handleClick}>
-                            Add To Cart
-                        </Button>
-                    </AddContainer>
-                </InfoContainer>
-            </Wrapper>
-        </Container>
-    );
+  const handleClick = (e) => {
+    e.preventDefault();
+    dispatch(addProduct({ ...product, quantity }));
+  };
+
+  return (
+    <Container>
+      <Wrapper>
+        <ImgContainer>
+          <Img src="https://i.ibb.co/Y0djRHf/product-1.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>{product?.title}</Title>
+          <Description>{product?.description}</Description>
+          <Price>$ {product?.price}</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black"></FilterColor>
+              <FilterColor color="tomato"></FilterColor>
+              <FilterColor color="cornflowerblue"></FilterColor>
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <IoIosRemove
+                onClick={() => quantity > 1 && setQuantity(quantity - 1)}
+              />
+              <Amount>{quantity}</Amount>
+              <IoIosAdd onClick={() => setQuantity(quantity + 1)} />
+            </AmountContainer>
+            <Button theme="dark" onClick={handleClick}>
+              Add To Cart
+            </Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+    </Container>
+  );
 };
